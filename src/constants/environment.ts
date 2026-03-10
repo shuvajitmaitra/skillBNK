5a556eb8704311b76845d3a2ff5f25fcc6eacbef
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2026-03-10 21:29:20 +0600

    3.15.10-------------------------------------------------

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -1,7 +1,7 @@
 export default {
   local: false,
   production: true,
-  version: '3.15.9',
+  version: '3.15.10',
   FRONTEND_URL: true
     ? 'http://192.168.68.114:4001/mobile'
     : 'https://portal.skillbnk.com/mobile',
